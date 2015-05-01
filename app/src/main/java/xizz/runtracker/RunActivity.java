d15e59521460db1854fe40adf24f15e51d1dd713
Xi Wei <xwei@amplify.com>
2015-05-01 16:46:09 -0400

    New project

---
+++ app/src/main/java/xizz/runtracker/RunActivity.java
@@ -0,0 +1,32 @@
+package xizz.runtracker;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class RunActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_run);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.menu_run, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		int id = item.getItemId();
+
+		if (id == R.id.action_settings)
+			return true;
+
+		return super.onOptionsItemSelected(item);
+	}
+}
