1a20ffdd57c0dfdf625d1cba3576ce07052766ce
Xi Wei <xwei@handy.com>
2015-10-16 12:08:52 -0400

    refactor

--- app/src/main/java/xizz/runtracker/LocationReceiver.java
+++ app/src/main/java/xizz/runtracker/LocationReceiver.java
@@ -25,19 +25,18 @@ public class LocationReceiver extends BroadcastReceiver {
 			loc.setTime(new Date().getTime());
 			RunManager.get(context).insertLocation(loc);
 			Log.d(TAG, "location saved");
-			Intent i = new Intent(context, RunActivity.class);
-			i.putExtra(RunActivity.EXTRA_RUN_ID, intent.getLongExtra(RunActivity.EXTRA_RUN_ID,
-					-1));
-			PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, i,
-					PendingIntent.FLAG_UPDATE_CURRENT);
+			Intent activityIntent = new Intent(context, RunActivity.class);
+			activityIntent.putExtra(RunActivity.EXTRA_RUN_ID, intent.getLongExtra(RunActivity.EXTRA_RUN_ID, -1));
+			PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, activityIntent,
+				PendingIntent.FLAG_UPDATE_CURRENT);
 			Notification.Builder builder = new Notification.Builder(context)
-					.setSmallIcon(android.R.drawable.ic_menu_mylocation)
-					.setContentTitle("New Location")
-					.setContentText(loc.getLatitude() + ", " + loc.getLongitude())
-					.setAutoCancel(true)
-					.setContentIntent(pendingIntent);
+				.setSmallIcon(android.R.drawable.ic_menu_mylocation)
+				.setContentTitle("New Location")
+				.setContentText(loc.getLatitude() + ", " + loc.getLongitude())
+				.setAutoCancel(true)
+				.setContentIntent(pendingIntent);
 			NotificationManager notificationManager =
-					(NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+				(NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
 			notificationManager.notify(0, builder.build());
 			broadcastLocation(context, intent.getLongExtra(RunActivity.EXTRA_RUN_ID, -1));
 		} else if (intent.hasExtra(LocationManager.KEY_PROVIDER_ENABLED)) {
@@ -48,7 +47,7 @@ public class LocationReceiver extends BroadcastReceiver {
 
 	protected void onLocationReceived(Context context, Location loc) {
 		Log.d(TAG, this + " Got location from " + loc.getProvider() + ": " + loc.getLatitude() +
-				", " + loc.getLongitude());
+			", " + loc.getLongitude());
 	}
 
 	protected void onProviderEnabledChanged(boolean enabled) {
